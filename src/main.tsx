eed0f28dfda347c829f4941c3a00b223ff422e9a
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-02 00:17:21 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
 import './i18n/config'
+import React from "react";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
