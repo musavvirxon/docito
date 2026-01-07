9062d8df99818e49e9ce92116de0c1d70980c786
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-07 18:19:09 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App.tsx";
+import "./index.css";
+import "./i18n/config";
+
+import { AuthProvider } from "@/contexts/AuthContext";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
