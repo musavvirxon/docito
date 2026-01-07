8c01329d8bbac1eef0ea58c6ddedf1afb4692391
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-07 18:23:36 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,26 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n/config";
 
 import { AuthProvider } from "@/contexts/AuthContext";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
