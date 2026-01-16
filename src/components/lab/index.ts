0aa3f408d60ea169a658fbc09392408b9062e8ed
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-16 23:59:35 +0500

    Update index.ts

--- src/components/lab/index.ts
+++ src/components/lab/index.ts
@@ -1,10 +1,15 @@
+// File: src/components/lab/index.ts
+
+export { default as LabAnalytics } from './LabAnalytics';
+export { default as LabBillingInsurance } from './LabBillingInsurance';
+export { default as LabHomeCollection } from './LabHomeCollection';
+export { LabManualTestOrderDialog } from './LabManualTestOrderDialog';
 export { LabOrderQueue } from './LabOrderQueue';
-export { TestCatalogManager } from './TestCatalogManager';
+export { LabReferralsSection } from './LabReferralsSection';
+export { default as LabSampleManager } from './LabSampleManager';
 export { LabStaffManager } from './LabStaffManager';
 export { ResultEntry } from './ResultEntry';
+export { TestCatalogManager } from './TestCatalogManager';
 export { TestOrderCreator } from './TestOrderCreator';
-export { default as LabSampleManager } from './LabSampleManager';
-export { default as LabHomeCollection } from './LabHomeCollection';
-export { default as LabBillingInsurance } from './LabBillingInsurance';
-export { default as LabAnalytics } from './LabAnalytics';
-export { LabReferralsSection } from './LabReferralsSection';
+export { TestParameterEditor } from './TestParameterEditor';
+export { LabSettingsSection } from './LabSettingsSection';
