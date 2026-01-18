154113efbf0f3ef51fd68b786986e580293dc7c7
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-18 12:22:14 +0500

    Create StaffDashboardPage.tsx

---
+++ src/pages/StaffDashboardPage.tsx
@@ -0,0 +1,5 @@
+// File: src/pages/StaffDashboardPage.tsx
+import StaffDashboardContent from "@/pages/StaffDashboardContent";
+export default function StaffDashboardPage() {
+  return <StaffDashboardContent />;
+}
