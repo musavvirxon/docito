cd7da12ffd10518e40b92238e68346f326af322e
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-18 13:29:42 +0500

    Update ImagingDashboardPage.tsx

--- src/pages/imaging/ImagingDashboardPage.tsx
+++ src/pages/imaging/ImagingDashboardPage.tsx
@@ -1,8 +1,8 @@
 // src/pages/imaging/ImagingDashboardPage.tsx
 // File: src/pages/imaging/ImagingDashboardPage.tsx
 
-import ImagingDashboard from "@/components/imaging/ImagingDashboard";
+import ImagingDashboardContent from "@/components/imaging/ImagingDashboard";
 
 export default function ImagingDashboardPage() {
-  return <ImagingDashboard />;
+  return <ImagingDashboardContent />;
 }
