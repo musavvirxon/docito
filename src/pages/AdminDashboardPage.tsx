351b1e877ac4655adf31a1a87ec2eb66fe83e8fa
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-18 12:21:05 +0500

    Create AdminDashboardPage.tsx

---
+++ src/pages/AdminDashboardPage.tsx
@@ -0,0 +1,5 @@
+// File: src/pages/AdminDashboardPage.tsx
+import AdminDashboardContent from "@/pages/AdminDashboardContent";
+export default function AdminDashboardPage() {
+  return <AdminDashboardContent />;
+}
