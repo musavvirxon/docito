09bbaab2a2aefb396c4a7182e0f85f9c3f96ed34
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-18 12:18:36 +0500

    Create ImagingDashboardPage.tsx

---
+++ src/pages/imaging/ImagingDashboardPage.tsx
@@ -0,0 +1,5 @@
+// File: src/pages/imaging/ImagingDashboardPage.tsx
+import ImagingDashboardContent from "@/pages/imaging/ImagingDashboardContent";
+export default function ImagingDashboardPage() {
+  return <ImagingDashboardContent />;
+}
