f78694c9490c3393cf492bc8be18e762e08adcc8
Musavvirkhon The Artist <69240986+musavvirxon@users.noreply.github.com>
2026-01-30 20:41:57 +0500

    Update index.ts

--- supabase/functions/user-settings/index.ts
+++ supabase/functions/user-settings/index.ts
@@ -1,4 +1,5 @@
-// supabase/functions/user-settings/index.ts
+// Path: supabase/functions/user-settings/index.ts
+// Path: supabase/functions/user-settings/index.ts
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
