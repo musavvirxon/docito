3ff718c214ea4b229908f3e8f44da30c7945954c
Musavvirxon Abduvoxidov <69240986+musavvirxon@users.noreply.github.com>
2026-03-01 18:26:14 +0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SITE_URL?: string;
+  readonly VITE_PUBLIC_SITE_URL?: string;
+  readonly VITE_APP_URL?: string;
+  readonly VITE_BLOG_DEFAULT_AUTHOR?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
