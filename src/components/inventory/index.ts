531519475f76db1f57d2d6c8d8aa7077fa90453c
Musavvirxon Abduvoxidov <69240986+musavvirxon@users.noreply.github.com>
2026-06-25 15:55:57 +0500

    Create index.ts

---
+++ src/components/inventory/index.ts
@@ -0,0 +1,5 @@
+// src/components/inventory/index.ts
+export { ClinicInventoryManager } from './ClinicInventoryManager';
+export { InventoryStockBadge } from './InventoryStockBadge';
+export { LowStockAlert } from './LowStockAlert';
+export { ProcedureInventoryItems } from './ProcedureInventoryItems';
