19ef844139f0ab2fe2034625496c12e118ef9360
Musavvirxon Abduvoxidov <69240986+musavvirxon@users.noreply.github.com>
2026-07-04 15:10:14 +0500

    Add files via upload

---
+++ src/components/practice/public/index.ts
@@ -0,0 +1,7 @@
+// src/components/practice/public/index.ts
+export { default as PracticeHeroSection }    from './PracticeHeroSection';
+export { default as PracticeStatsBar }       from './PracticeStatsBar';
+export { default as PracticeAboutSection }   from './PracticeAboutSection';
+export { default as PracticeDoctorsSection } from './PracticeDoctorsSection';
+export { default as PracticeContactSection } from './PracticeContactSection';
+export { default as PracticeTrustSection }   from './PracticeTrustSection';
